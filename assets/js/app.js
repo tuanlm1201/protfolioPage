abd777e093fa74a2d54e9c2c13bb666452d77b7c
tuanlm1201 <83169461+tuanlm1201@users.noreply.github.com>
2021-10-22 20:18:49 +0700

    Update Berries

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,20 +1,23 @@
-const navbar = document.querySelector('.navbar');
+//After Scroll Navbar
+const navbar = document.querySelector(".navbar");
+const navLogo = document.querySelector(".navbar__logo");
 
-window.onscroll = ()=>{
-    if(document.body.scrollTop > 20 || document.documentElement.scrollTop> 20){
-        Object.assign(navbar.style, {
-            position: 'fixed',
-            top: 0,
-            right: 0,
-            left: 0,
-            background: 'black',
-            marginRight: '50px',
-            Transition: 'all .3s'
-        })
-    }else{
-        Object.assign(navbar.style, {
-            position: 'relative',
-            background: 'none',
-        })
-    }
-}
+window.onscroll = () => {
+  if (document.body.scrollTop > 46 || document.documentElement.scrollTop > 46) {
+    Object.assign(navbar.style, {
+      position: "fixed",
+      top: 0,
+      right: 0,
+      left: 0,
+      background: "black",
+      transition: ".3s linear",
+    });
+    navLogo.style.display = "none";
+  } else {
+    navLogo.style.display = "block";
+    Object.assign(navbar.style, {
+      background: "transparent",
+      top: 0,
+    });
+  }
+};
